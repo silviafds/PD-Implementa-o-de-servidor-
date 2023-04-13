f9cd662fba8556b8cfb64522021167a54460abe0
silviafds <silviafernandes974@gmail.com>
2023-04-13 20:13:36 -0300

    correção na classe de autenticação, erro de bind no servidor e classe usuario criada

--- src/autenticacao/RMI/Autenticacao.java
+++ src/autenticacao/RMI/Autenticacao.java
@@ -5,13 +5,11 @@ import java.rmi.RemoteException;
 public interface Autenticacao {
 	
 
-	 // método que cadastra novo usuário
-    public void registrar(String nome, String senha, String permissao) throws RemoteException;
+	  // método que cadastra novo usuário
+    public void registraUsuario(String nome, String senha, String permissao) throws RemoteException;
 
-    // método no qual autentica um usuário
-    public boolean autenticacao(String nome, String senha) throws RemoteException;
+    // método que autentica um usuário e o autoriza a operar um objeto
+    boolean solicitaAcesso(String nome, String senha, String objeto, String operacao) throws RemoteException;
 
-    // método para verificar a possibilidade de um usuário possur permissão para acessar um objeto
-    public boolean autorizacao(String nome, String objeto, String permissao) throws RemoteException;
     
 }
---
+++ src/autenticacao/RMI/Usuario.java
@@ -0,0 +1,40 @@
+package autenticacao.RMI;
+
+public class Usuario {
+	
+	private String nome;
+	private String senha;
+	private String autorizacao;
+	
+	public Usuario(String nome, String senha, String autorizacao) {
+		super();
+		this.nome = nome;
+		this.senha = senha;
+		this.autorizacao = autorizacao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getAutorizacao() {
+		return autorizacao;
+	}
+
+	public void setAutorizacao(String autorizacao) {
+		this.autorizacao = autorizacao;
+	}
+
+}
